4446aab991fa9026d2ec080368793ed96b8a90c9
WuXinxinJH <wuxinxinjh@163.com>
2015-09-20 16:29:45 +0800

    add location

---
+++ Demo2.java
@@ -0,0 +1,20 @@
+package pm;
+
+import java.awt.Color;
+
+public class Demo2 {
+	//Иёзг
+	int x,y;
+	Color col;
+	public Demo2(){
+		
+	}
+	
+			
+	public Demo2(int x, int y, Color col) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.col = col;
+	}
+}
